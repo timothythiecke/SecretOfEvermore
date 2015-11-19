c37acd7754673d66d77d8d198c7d3e645f9d4173
timothythiecke <timothy.thiecke@gmail.com>
2015-11-19 22:57:19 +0100

    Looting system, items found added to inventory

---
+++ SecretOfEvermore/Assets/Scripts/Loot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Items;
+
+public class Loot : MonoBehaviour {
+
+    private Item _lootItem;
+
+    void Start()
+    {
+        int random = Random.Range(0, 101);
+        if ((random >= 0) && (random <= 25))
+        {
+            _lootItem = new Armor();
+        }
+
+        if ((random >= 26) && (random <= 50))
+        {
+            _lootItem = new Bow();
+        }
+
+        if ((random >= 51) && (random <= 75))
+        {
+            _lootItem = new Staff();
+        }
+
+        if ((random >= 76) && (random <= 100))
+        {
+            _lootItem = new Sword();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Pickup & destroy
+                (Instantiate(Resources.Load("TextPrefab"), transform.position + new Vector3(0, 2.5f, 0), new Quaternion()) as GameObject).GetComponent<TextMesh>().text = _lootItem.ToString() + " looted!";
+                GameManager.Instance.Inventory.AddToInventory(_lootItem);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ SecretOfEvermore/Assets/Scripts/MoveTextUpwards.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTextUpwards : MonoBehaviour {
+
+    void Start()
+    {
+        Destroy(gameObject, 4F);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    // v = a * t
+        // s = v * t
+
+        float speed = 70 * Time.deltaTime;
+
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+	}
+}
